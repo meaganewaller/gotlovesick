06ff494bc1883fcc4486baf7071a5cdb2e12276d
Meagan Waller <meagan@meaganwaller.com>
2025-01-23 18:54:18 -0500

    Fix issues with pagination on blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,112 +1,64 @@
+import { Pagination } from "@/components/blog";
 import { fetchAllPosts } from "@/services/graphql";
 import { Post } from "@/types";
+import { formatDateAsString } from "@/utils/helpers";
+import '@/styles/blog.css';
+import Link from "next/link";
 
-interface Props {
-  posts: Post[]
+type SearchParams = {
+  page?: string | null;
 }
 
-export default function BlogIndex({ posts }: Props) {
+type Props = {
+  searchParams: SearchParams
+}
+
+const Page = async ({ searchParams }: Props) => {
+  const perPage = 10;
+  let page = searchParams.page ? parseInt(searchParams.page) : 1;
+  let currentPage = page;
+  let totalPages = 1;
+
+  const response = await fetchAllPosts(page, perPage)
+
+  if (response.posts?.pageInfo?.offsetPagination?.total) {
+    totalPages = Math.ceil(response.posts.pageInfo.offsetPagination.total / perPage);
+  }
+
+  const posts = response.posts?.nodes;
+
+  if (posts?.length === 0) {
+    return (
+      <div>
+        <h1>No posts found</h1>
+      </div>
+    )
+  }
+
   return (
     <main id="blog-page">
       <div className='layout'>
-        <header className="header">
-          <section className="about">
-            <h1>Blog Page 1</h1>
-            <p>Discover the latest posts and updates.</p>
+        <header className='header'>
+          <section className='about-section'>
+            <h1>Blog Posts (page {page})</h1>
           </section>
         </header>
 
-        {/* Render the Grid component */}
-        {/* <Grid initialItems={posts} columnGutter={10} columnWidth={325} /> */}
+        <section className="posts">
+          {posts?.map((post: Post) => (
+            <div key={post.key}>
+              <h1>{post.title}</h1>
+              <span>{formatDateAsString(post.date)}</span>
+              <p>{post.postDetails?.description}</p>
+              <Link href={`/blog/${post.slug}`}>Read more</Link>
+            </div>
+          ))}
+        </section>
 
-        {/* Render the Pagination component */}
-        {/* <Pagination currentPage={currentPage} totalPages={totalPages} /> */}
+        <Pagination currentPage={currentPage} totalPages={totalPages} />
       </div>
     </main>
   )
 }
-// import { notFound } from 'next/navigation';
-// import { fetchAllPosts } from '@/services/graphql';
-// import '@/styles/blog.css';
-// import { Grid, Pagination } from '@/components/blog';
 
-// type BlogIndexPageProps = {
-//   searchParams?: {
-//     page?: string;
-//   }
-// };
-
-// export default async function BlogIndexPage({ searchParams }: BlogIndexPageProps) {
-//   const perPage = 10;
-//   const currentPage = parseInt(searchParams?.page || '1', 10);
-
-//   const response = await fetchAllPosts(currentPage, perPage);
-
-//   if (!response || !response.posts || response.posts.nodes.length === 0) {
-//     return notFound();
-//   }
-
-//   const posts = response.posts.nodes;
-//   const totalPosts = response.posts.pageInfo.offsetPagination.total;
-//   const totalPages = Math.ceil(totalPosts / perPage);
-
-//   return (
-//     <main id="blog-page">
-//       <div className='layout'>
-//         <header className="header">
-//           <section className="about">
-//             <h1>Blog Page {currentPage}</h1>
-//             <p>Discover the latest posts and updates.</p>
-//           </section>
-//         </header>
-
-//         {/* Render the Grid component */}
-//         <Grid initialItems={posts} columnGutter={10} columnWidth={325} />
-
-//         {/* Render the Pagination component */}
-//         <Pagination currentPage={currentPage} totalPages={totalPages} />
-//       </div>
-//     </main>
-//   )
-// }
-
-// // export default async function BlogIndexPage({ searchParams }: BlogIndexPageProps) {
-// //   const params = await searchParams;
-
-// //   const postsPerPage = 10;
-// //   const currentPage = parseInt(params.page || '1', 10);
-
-// //   const initialData = await fetchAllPosts(currentPage, postsPerPage);
-
-// //   if (!initialData || initialData.data.posts.edges.length === 0) {
-// //     return notFound();
-// //   }
-
-// //   const { pageInfo, edges } = initialData.data.posts;
-
-// //   if (!pageInfo || !edges) {
-// //     return notFound();
-// //   }
-
-// //   console.log('Page Info:', pageInfo);
-// //   console.log('Edges:', edges);
-
-// //   const totalPosts = pageInfo.offsetPagination.total;
-// //   const totalPages = Math.ceil(totalPosts / postsPerPage);
-
-// //   return (
-// //     <main id="blog-page">
-// //       <div className="layout">
-// //         <header className="header">
-// //           <section className="about">
-// //             <h1>Blog</h1>
-// //             <p>Some blog description will live here.</p>
-// //           </section>
-// //         </header>
-
-// //         <Grid initialItems={edges} columnGutter={10} columnWidth={325} />
-// //         <Pagination currentPage={currentPage} totalPages={totalPages} />
-// //       </div>
-// //     </main>
-// //   );
-// // }
+export default Page;
